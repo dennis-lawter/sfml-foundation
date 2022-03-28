fe041052f650d0edc4214f9f00662b0752f08eb0
bryanbarreto <bryan94barreto@gmail.com>
2022-03-28 01:21:41 -0400

    vector normalization

--- src/playerInput.hh
+++ src/playerInput.hh
@@ -4,11 +4,12 @@
 #include <algorithm>
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include "util.hh"
 
 namespace playerInput {
-
-	sf::Vector2f inputSideScroll(sf::Event& event);
-	sf::Vector2f inputTopDown(sf::Event& event);
+	sf::Vector2f getInputVector();
+	void inputSideScroll(sf::Event& event);
+	void inputTopDown(sf::Event& event);
 }
 
 #endif
