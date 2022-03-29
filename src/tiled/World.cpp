360d7eeaf5a7a9733dd50a86f6b818788547754a
Dennis Lawter <bytomancer@gmail.com>
2022-03-29 11:36:47 -0400

    Setup ase files, working toward static collision

--- src/tiled/World.cpp
+++ src/tiled/World.cpp
@@ -29,23 +29,27 @@ void World::loadFromJson() {
 
 	for (auto& layerJson : worldJson["layers"]) {
 		if (layerJson["type"] == "tilelayer") {
-			nlohmann::json tileData = layerJson["data"];
-			for (unsigned int y = 0; y < this->height; y++) {
-				for (unsigned int x = 0; x < this->width; x++) {
-					unsigned int tileId = tileData[this->width * y + x];
-					if (tileId == 0) continue;
+			if (layerJson["name"] == "collision") {
+				//
+			} else {
+				nlohmann::json tileData = layerJson["data"];
+				for (unsigned int y = 0; y < this->height; y++) {
+					for (unsigned int x = 0; x < this->width; x++) {
+						unsigned int tileId = tileData[this->width * y + x];
+						if (tileId == 0) continue;
 
-					unsigned char tileFlags = (tileId & MOST_SIGNIFICANT_BYTE) >> 24;
-					tileId &= THREE_LEAST_SIGNIFICANT_BYTES;
+						unsigned char tileFlags = (tileId & MOST_SIGNIFICANT_BYTE) >> 24;
+						tileId &= THREE_LEAST_SIGNIFICANT_BYTES;
 
-					unsigned int tilesIndex = this->tiles.size();
-					std::string tileName = this->idToTileNames[tileId];
-					this->tiles.emplace_back(tileName, this->tileSets[tileName]);
-					this->tiles[tilesIndex].transformation = (Tile::Transformation)tileFlags;
-					this->tiles[tilesIndex].setPosition(x * this->tileWidth + this->tileWidth / 2.f, y * this->tileHeight + this->tileHeight / 2.f);
-					this->tiles[tilesIndex].setOrigin(this->tileWidth / 2.f, this->tileHeight / 2.f);
-					this->tiles[tilesIndex].setTileFromId(tileId);
-					this->tiles[tilesIndex].performTransformations();
+						unsigned int tilesIndex = this->tiles.size();
+						std::string tileName = this->idToTileNames[tileId];
+						this->tiles.emplace_back(tileName, this->tileSets[tileName]);
+						this->tiles[tilesIndex].transformation = (Tile::Transformation)tileFlags;
+						this->tiles[tilesIndex].setPosition(x * this->tileWidth + this->tileWidth / 2.f, y * this->tileHeight + this->tileHeight / 2.f);
+						this->tiles[tilesIndex].setOrigin(this->tileWidth / 2.f, this->tileHeight / 2.f);
+						this->tiles[tilesIndex].setTileFromId(tileId);
+						this->tiles[tilesIndex].performTransformations();
+					}
 				}
 			}
 		}
