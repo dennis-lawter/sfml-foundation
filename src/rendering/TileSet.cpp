52f340aa206394d2cd0b84dd2ef1ded81aee1406
Dennis Lawter <bytomancer@gmail.com>
2022-03-29 00:42:45 -0400

    Tile mapping errors

--- src/rendering/TileSet.cpp
+++ src/rendering/TileSet.cpp
@@ -5,10 +5,10 @@ TileSet::TileSet() {
 
 sf::IntRect TileSet::getTileBox(sf::Vector2i coordPair) {
 	sf::IntRect rect;
-	unsigned int rows = this->tileCount / this->columns;
+	// unsigned int rows = this->tileCount / this->columns;
 
-	rect.left = coordPair.x * this->columns;
-	rect.top = coordPair.y * rows;
+	rect.left = coordPair.x * this->tileWidth;
+	rect.top = coordPair.y * this->tileHeight;
 	rect.width = this->tileWidth;
 	rect.height = this->tileHeight;
 
@@ -17,10 +17,10 @@ sf::IntRect TileSet::getTileBox(sf::Vector2i coordPair) {
 
 sf::IntRect TileSet::getTileBoxById(unsigned int id) {
 	unsigned int idOffset = id - this->firstGid;
-	unsigned int rows = this->tileCount / this->columns;
-	unsigned int rowSelected = idOffset / rows;
+	// unsigned int rows = this->tileCount / this->columns;
+	unsigned int rowSelected = idOffset / this->columns;
 	unsigned int colSelected = idOffset % this->columns;
-	sf::Vector2i coordPair (rowSelected, colSelected);
+	sf::Vector2i coordPair (colSelected, rowSelected);
 	return this->getTileBox(coordPair);
 }
 
